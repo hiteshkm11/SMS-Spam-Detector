ed26d241282dda29a288aae9f6e20c0863a64706
hiteshkm11 <hiteshadakankar111@gmail.com>
2024-03-16 18:16:42 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
 
+
+#importing the essential libraries
+from flask import Flask , render_template , request
+import pickle
+import numpy as np
+
+#loading the saved models
+model = pickle.load(open('model.pkl' , 'rb'))
+cv = pickle.load(open('vector.pkl' , 'rb'))
+
+#creating flask instance
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+#home page flask code
+@app.route('/')
+def home():
+  return render_template('home.html')
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+
+#prediction page flask code
+@app.route('/predict' , methods=['POST'])
+def predict():
+  message = request.form['text']
+  data = [message]
+  data = cv.transform(data).toarray()
+  pred = model.predict(data)
+  return render_template('result.html' , prediction = pred)
+
+
+#Running the flask app
+if __name__ == '__main__':
+  app.run(host="0.0.0.0")
